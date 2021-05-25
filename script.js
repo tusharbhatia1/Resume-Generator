20c003c258eac034e336acc6e3eb548a7917a343
tusharbhatia1 <tusharbhatia655@gmail.com>
2021-05-25 16:39:10 +0530

    Basic modal created

---
+++ script.js
@@ -0,0 +1,41 @@
+function addNewWE(){
+    let newNode = document.createElement('textarea');
+    newNode.classList.add('form-control');
+    newNode.classList.add('wefield');
+    newNode.classList.add('mt-2');
+    newNode.setAttribute("rows", 3);
+    newNode.setAttribute("placeholder", "Enter Your Work Experience");
+
+    let WEOb = document.getElementById("WE");
+    let weAddBtnOb = document.getElementById("weAddbBtn");
+
+    WEOb.insertBefore(newNode, weAddBtnOb);
+}
+
+function addNewAQ(){
+    let newNode = document.createElement('textarea');
+    newNode.classList.add('form-control');
+    newNode.classList.add('aqfield');
+    newNode.classList.add('mt-2');
+    newNode.setAttribute("rows", 3);
+    newNode.setAttribute("placeholder", "Enter Your Educational Qualification");
+
+    let AQOb = document.getElementById("AQ");
+    let aqAddBtnOb = document.getElementById("aqAddbBtn");
+
+    AQOb.insertBefore(newNode, aqAddBtnOb);
+}
+
+function addNewCER(){
+    let newNode = document.createElement('textarea');
+    newNode.classList.add('form-control');
+    newNode.classList.add('cerfield');
+    newNode.classList.add('mt-2');
+    newNode.setAttribute("rows", 2);
+    newNode.setAttribute("placeholder", "Enter Your Certifications");
+
+    let CEROb = document.getElementById("CER");
+    let cerAddBtnOb = document.getElementById("cerAddbBtn");
+
+    CEROb.insertBefore(newNode, cerAddBtnOb);
+}
